882cedd4f2c910f642a397f72129cd8c52652a8b
Oleksandr Kaiumov <66486758+Redpaw-Game-Dev@users.noreply.github.com>
2023-01-16 10:18:15 +0200

    Initial commit

---
+++ Assets/Scripts/Loading/LoadingManager.cs
@@ -0,0 +1,38 @@
+﻿using Cysharp.Threading.Tasks;
+using UnityEngine.AddressableAssets;
+using UnityEngine.SceneManagement;
+using Zenject;
+
+namespace Scripts.Loading
+{
+    public class LoadingManager : IInitializable
+    {
+        private bool _isBaseScenesLoading;
+        private bool _isBaseScenesLoaded;
+        private BaseScenesConfig _baseScenesConfig;
+        
+        private LoadingManager(BaseScenesConfig baseScenesConfig)
+        {
+            _baseScenesConfig = baseScenesConfig;
+        }
+        
+        public void Initialize()
+        {
+            LoadBaseScenes();
+        }
+
+        private async void LoadBaseScenes()
+        {
+            if(_baseScenesConfig == null || _isBaseScenesLoaded || _isBaseScenesLoading) return;
+            
+            _isBaseScenesLoading = true;
+            AssetReference[] baseScenes = _baseScenesConfig.BaseScenes;
+            foreach (var scene in baseScenes)
+            {
+                await scene.LoadSceneAsync(LoadSceneMode.Additive);
+            }
+            _isBaseScenesLoading = false;
+            _isBaseScenesLoaded = true;
+        }
+    }
+}
---
+++ Assets/Scripts/Loading/LoadingManagerInstaller.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using Zenject;
+
+namespace Scripts.Loading
+{
+    public class LoadingManagerInstaller : MonoInstaller
+    {
+        [SerializeField] protected BaseScenesConfig _baseScenesConfig;
+        
+        public override void InstallBindings()
+        {
+            Container.BindInterfacesAndSelfTo<LoadingManager>().FromNew().AsSingle().WithArguments(_baseScenesConfig);
+        }
+    }
+}
